6bcff56d3b310163fc4879eb44498e3996aa1645
charles65536 <zxszeto656@connect.hkust-gz.edu.cn>
2024-11-28 20:29:47 +0800

    Initial Commit

---
+++ miniSql_proj_finalPush/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  miniSql_proj_finalPush
+//
+//  Created by 司徒健飞 on 2024/11/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
