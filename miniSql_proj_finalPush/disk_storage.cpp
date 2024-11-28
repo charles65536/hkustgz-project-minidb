aa732467c281ca47612c39e3856c668ae45199e7
charles65536 <zxszeto656@connect.hkust-gz.edu.cn>
2024-11-29 00:31:43 +0800

    update: store database on disk

---
+++ miniSql_proj_finalPush/disk_storage.cpp
@@ -0,0 +1,8 @@
+//
+//  disk_storage.cpp
+//  miniSql_proj_finalPush
+//
+//  Created by 司徒健飞 on 2024/11/28.
+//
+
+#include "disk_storage.hpp"
---
+++ miniSql_proj_finalPush/disk_storage.hpp
@@ -0,0 +1,13 @@
+//
+//  disk_storage.hpp
+//  miniSql_proj_finalPush
+//
+//  Created by 司徒健飞 on 2024/11/28.
+//
+
+#ifndef disk_storage_hpp
+#define disk_storage_hpp
+
+#include <stdio.h>
+
+#endif /* disk_storage_hpp */
